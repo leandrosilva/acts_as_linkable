fba82b1ac945a3b413bb1a87dd85518d72d0472f
Leandro Silva <leandrodoze@gmail.com>
2010-03-12 21:12:36 -0800

    refactoring

--- lib/acts_as_linkable.rb
+++ lib/acts_as_linkable.rb
@@ -8,6 +8,7 @@ module CodeZone
       module ClassMethods
         def acts_as_linkable
           include InstanceMethods
+          extend SingletonMethods
         end
       end
   
@@ -15,28 +16,53 @@ module CodeZone
         def link_to(link_spec = nil)
           link_spec.each_pair do |name, uri|
             raise ArgumentError.new('Argument must be :name => "uri".') unless name && uri
-            
-            define_link_methods(name, uri)
+
+            links[name] = uri
           end
-        end
-        
-        def is_linked_to?(link_name)
-          respond_to? "#{link_name}_link"
-        end
-        
-        def go_to_link!(link_name)
-          puts "TODO should submit to #{name} (#{uri})."
         end
 
-        private
-  
-          def define_link_methods(name, uri)
-            class_eval do
-              send(:define_method, "#{name}_link") do
-                uri
-              end
-            end
+        def links
+          @links ||= {}
+        end
+        
+        def is_linked_to?(name)
+          links.include? name
+        end
+        
+        def go_to_link!(name)
+          puts "TODO should submit to #{name} (#{uri})."
+        end
+        
+        def to_xml(args = {})
+          xml = super(args)
+
+          xml_doc = Nokogiri::XML(xml)
+
+          links.each_pair do |name, uri|
+            link_node = Nokogiri::XML::Node.new('atom:link', xml_doc)
+            link_node['rel'] = name.to_s
+            link_node['href'] = uri
+            link_node['type'] = 'application/xml'
+
+            xml_doc.root << link_node
           end
+
+          xml = xml_doc.to_xml
+        end
+      end
+      
+      module SingletonMethods
+        def from_xml(xml)
+          payment = super(xml)
+
+          xml_doc = Nokogiri::XML(xml)
+          
+          xml_doc.xpath('//link').each do |link_node|
+            payment.link_to link_node['rel'].to_sym => link_node['href']
+          end
+          
+          payment
+        end
       end
     end
   end
